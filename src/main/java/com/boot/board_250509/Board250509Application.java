7acd1c265c9e93b10a338983e413bc454e630da5
carlton777 <sangkoo@naver.com>
2025-05-09 10:08:10 +0900

    1

---
+++ src/main/java/com/boot/board_250509/Board250509Application.java
@@ -0,0 +1,13 @@
+package com.boot.board_250509;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board250509Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board250509Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/board_250509/Board250509ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.board_250509;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board250509ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
